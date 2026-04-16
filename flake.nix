bc9f320f26164bbe2244b22bff286c1e87bb7d4d
REVO9 <luisdanker@gmx.de>
2026-04-16 15:44:24 +0200

    bumb version

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
   in {
     packages.${system}.default = pkgs.rustPlatform.buildRustPackage rec {
       pname = "lecture-mgr";
-      version = "0.1.0";
+      version = "0.2.0";
       src = ./.;
       cargoLock = {
         lockFile = ./Cargo.lock;
